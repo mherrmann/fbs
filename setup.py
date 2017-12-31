aca6cbd83e6a1d3f839c4b819caf5ef7ae53ceb1
Michael Herrmann <michael@herrmann.io>
2017-12-31 11:16:34 +0100

    Add missing data files to PyPI

--- setup.py
+++ setup.py
@@ -4,12 +4,22 @@ See:
 https://github.com/mherrmann/fbs
 """
 
+from glob import glob
+from os.path import relpath, join
+
 from setuptools import setup, find_packages
+
+def _get_package_data(pkg_dir, data_subdir):
+	glob_pattern = join(pkg_dir, data_subdir, '**', '*.*')
+	return [
+		relpath(file_path, pkg_dir)
+		for file_path in glob(glob_pattern, recursive=True)
+	]
 
 description = 'Easily create cross-platform desktop applications with PyQt'
 setup(
 	name='fbs',
-	version='0.0.3',
+	version='0.0.4',
 	description=description,
 	long_description=
 		description + '\n\nHome page: https://github.com/mherrmann/fbs',
@@ -17,7 +27,9 @@ setup(
 	author_email='michael+removethisifyouarehuman@herrmann.io',
 	url='https://github.com/mherrmann/fbs',
 	packages=find_packages(),
-	package_data={'fbs': ['default_settings']},
+	package_data={
+		'fbs': _get_package_data('fbs', 'default_settings')
+	},
 	classifiers=[
 		'Development Status :: 2 - Pre-Alpha',
 		'Intended Audience :: Developers',
