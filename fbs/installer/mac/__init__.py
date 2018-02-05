83aaaf602fb8906d04bc015d9190ea6aac00508a
Michael Herrmann <michael@herrmann.io>
2018-02-05 18:20:19 +0100

    Fix small problem in Mac 'installer' command

---
+++ fbs/installer/mac/__init__.py
@@ -0,0 +1,14 @@
+from fbs import path, SETTINGS
+from os.path import join, dirname
+from subprocess import run
+
+def create_installer_mac():
+    app_name = SETTINGS['app_name']
+    run([
+        join(dirname(__file__), 'yoursway-create-dmg', 'create-dmg'),
+        '--volname', app_name,
+        '--app-drop-link', '0', '10',
+        '--icon', app_name + '.app', '200', '10',
+        path('target/${app_name}.dmg'),
+        path('${freeze_dir}')
+    ], check=True)
