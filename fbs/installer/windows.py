816ee6963ec92fc771626f5b48331c12edd2b9b4
Michael Herrmann <michael@herrmann.io>
2018-02-28 10:51:12 +0100

    Move Windows installer script into fbs-tutorial
    This lets fbs users customize the installer.

--- fbs/installer/windows.py
+++ fbs/installer/windows.py
@@ -1,22 +1,21 @@
 from fbs import path, SETTINGS
 from fbs.resources import copy_with_filtering
-from os.path import join, dirname
 from subprocess import run
 
 def create_installer_windows():
-	setup_nsi = join(dirname(__file__), 'Setup.nsi')
+	installer_nsi = path('src/main/Installer.nsi')
 	copy_with_filtering(
-		setup_nsi,
+		installer_nsi,
 		path('target/NSIS'),
 		replacements={
 			'app_name': SETTINGS['app_name'],
 			'author': SETTINGS['author']
 		},
-		files_to_filter=[setup_nsi],
+		files_to_filter=[installer_nsi],
 		placeholder='%%{%s}'
 	)
 	try:
-		run(['makensis', 'Setup.nsi'], cwd=path('target/NSIS'), check=True)
+		run(['makensis', 'Installer.nsi'], cwd=path('target/NSIS'), check=True)
 	except FileNotFoundError:
 		raise FileNotFoundError(
 			"fbs could not find executable 'makensis'. Please install NSIS and "
