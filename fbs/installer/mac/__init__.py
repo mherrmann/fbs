fea21a9ba39137139bd703e8992775b7e6a0d073
Michael Herrmann <michael@herrmann.io>
2018-04-17 14:57:56 +0200

    Improve Mac installer icon placement

--- fbs/installer/mac/__init__.py
+++ fbs/installer/mac/__init__.py
@@ -7,8 +7,8 @@ def create_installer_mac():
     run([
         join(dirname(__file__), 'yoursway-create-dmg', 'create-dmg'),
         '--volname', app_name,
-        '--app-drop-link', '0', '10',
-        '--icon', app_name + '.app', '200', '10',
+        '--app-drop-link', '170', '10',
+        '--icon', app_name + '.app', '0', '10',
         path('target/${app_name}.dmg'),
         path('${freeze_dir}')
     ], check=True)
