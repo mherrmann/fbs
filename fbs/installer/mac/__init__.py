74e12be1e14cea915cb535f5bf4cb16176ba7261
Michael Herrmann <michael@herrmann.io>
2018-10-01 09:15:53 +0200

    Don't require `clean` between 2x `installer`

--- fbs/installer/mac/__init__.py
+++ fbs/installer/mac/__init__.py
@@ -1,14 +1,25 @@
 from fbs import path, SETTINGS
-from os.path import join, dirname
+from os import replace
+from os.path import join, dirname, exists
 from subprocess import run
 
 def create_installer_mac():
     app_name = SETTINGS['app_name']
-    run([
-        join(dirname(__file__), 'yoursway-create-dmg', 'create-dmg'),
-        '--volname', app_name,
-        '--app-drop-link', '170', '10',
-        '--icon', app_name + '.app', '0', '10',
-        path('target/${app_name}.dmg'),
-        path('${freeze_dir}')
-    ], check=True)
+    dest = path('target/${app_name}.dmg')
+    dest_existed = exists(dest)
+    if dest_existed:
+        dest_bu = dest + '.bu'
+        replace(dest, dest_bu)
+    try:
+        run([
+            join(dirname(__file__), 'yoursway-create-dmg', 'create-dmg'),
+            '--volname', app_name,
+            '--app-drop-link', '170', '10',
+            '--icon', app_name + '.app', '0', '10',
+            dest,
+            path('${freeze_dir}')
+        ], check=True)
+    except:
+        if dest_existed:
+            replace(dest_bu, dest)
+        raise
