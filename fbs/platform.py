a7c07c616770bddd9ba9ed7c9433c54b46fa1afe
Michael Herrmann <michael@herrmann.io>
2018-10-09 12:01:10 +0200

    Add fbs.platform.is_fedora()

--- fbs/platform.py
+++ fbs/platform.py
@@ -2,9 +2,27 @@
 from fbs_runtime.system import is_windows, is_mac, is_linux, name
 
 def is_ubuntu():
-    with open('/etc/issue', 'r') as f:
-        return f.read().startswith('Ubuntu ')
+    try:
+        return _get_os_release_name().startswith('Ubuntu')
+    except FileNotFoundError:
+        return False
 
 def is_arch_linux():
-    with open('/etc/issue', 'r') as f:
-        return f.read().startswith('Arch Linux ')
+    try:
+        return _get_os_release_name().startswith('Arch Linux')
+    except FileNotFoundError:
+        return False
+
+def is_fedora():
+    try:
+        return _get_os_release_name().startswith('Fedora')
+    except FileNotFoundError:
+        return False
+
+def _get_os_release_name():
+    with open('/etc/os-release', 'r') as f:
+        for line in f:
+            line = line.rstrip()
+            if line.startswith('NAME='):
+                name = line[len('NAME='):]
+                return name.strip('"')
