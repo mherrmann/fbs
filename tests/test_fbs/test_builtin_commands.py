9e5b4ed6e532bc608666adc70a10857ac35be041
Michael Herrmann <michael@herrmann.io>
2018-10-19 15:37:55 +0200

    Add a test for `freeze` and `installer`

---
+++ tests/test_fbs/test_builtin_commands.py
@@ -0,0 +1,19 @@
+from fbs import path
+from fbs.builtin_commands import freeze, installer
+from fbs_runtime.platform import is_mac
+from os.path import exists
+from tests.test_fbs import FbsTest
+
+class BuiltInCommandsTest(FbsTest):
+    def test_freeze_installer(self):
+        freeze()
+        if is_mac():
+            executable = path('${freeze_dir}/Contents/MacOS/${app_name}')
+        else:
+            executable = path('${freeze_dir}/${app_name}')
+        self.assertTrue(exists(executable), executable + ' does not exist')
+        installer()
+        self.assertTrue(exists(path('target/${installer}')))
+    def setUp(self):
+        super().setUp()
+        self.init_fbs()
