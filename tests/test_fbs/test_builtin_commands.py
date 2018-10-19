dc03cd3771df3735668969f1fcee4794e0eb6ebc
Michael Herrmann <michael@herrmann.io>
2018-10-19 16:18:14 +0200

    Fix broken test on Windows

--- tests/test_fbs/test_builtin_commands.py
+++ tests/test_fbs/test_builtin_commands.py
@@ -1,6 +1,6 @@
 from fbs import path
 from fbs.builtin_commands import freeze, installer
-from fbs_runtime.platform import is_mac
+from fbs_runtime.platform import is_mac, is_windows
 from os.path import exists
 from tests.test_fbs import FbsTest
 
@@ -9,6 +9,8 @@ class BuiltInCommandsTest(FbsTest):
         freeze()
         if is_mac():
             executable = path('${freeze_dir}/Contents/MacOS/${app_name}')
+        elif is_windows():
+            executable = path('${freeze_dir}/${app_name}.exe')
         else:
             executable = path('${freeze_dir}/${app_name}')
         self.assertTrue(exists(executable), executable + ' does not exist')
