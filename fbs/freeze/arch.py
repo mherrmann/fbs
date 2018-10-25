b4b0a565a92cbc8ba699ceef61994e37f0e168d0
Michael Herrmann <michael@herrmann.io>
2018-10-25 09:30:19 +0200

    Fix: The frozen app wouldn't start on Arch

--- fbs/freeze/arch.py
+++ fbs/freeze/arch.py
@@ -1,10 +1,4 @@
-from fbs.freeze.linux import freeze_linux, remove_shared_libraries
+from fbs.freeze.linux import freeze_linux
 
 def freeze_arch(extra_pyinstaller_args=None, debug=False):
     freeze_linux(extra_pyinstaller_args, debug)
-    # Our apps normally ship with eg. libQt5Core.so.5. This loads other .so
-    # files, if present, from /usr/lib. If those libraries are Qt libraries of a
-    # different Qt version, errors occur.
-    # For this reason, on systems with pacman, we don't include Qt. Instead, we
-    # declare it as a dependency and leave it up to pacman to fetch it.
-    remove_shared_libraries('libicudata.so.*', 'libQt*.so.*')
