b8f2875b31554af885ffda7cc2c4c0a581eec39b
Michael Herrmann <michael@herrmann.io>
2018-10-30 15:30:53 +0100

    Reduce output of `installer` on Windows

--- fbs/installer/windows.py
+++ fbs/installer/windows.py
@@ -1,11 +1,14 @@
 from fbs import path
 from fbs.installer import _generate_installer_resources
-from subprocess import check_call
+from subprocess import check_call, DEVNULL
 
 def create_installer_windows():
     _generate_installer_resources()
     try:
-        check_call(['makensis', 'Installer.nsi'], cwd=path('target/installer'))
+        check_call(
+            ['makensis', 'Installer.nsi'], cwd=path('target/installer'),
+            stdout=DEVNULL
+        )
     except FileNotFoundError:
         raise FileNotFoundError(
             "fbs could not find executable 'makensis'. Please install NSIS and "
