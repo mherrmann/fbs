135b93110d6e3f1e4ae3e048619e383c6624c5d5
Michael Herrmann <michael@herrmann.io>
2018-11-10 10:59:12 +0100

    Set version number for release 0.3.9

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def _get_package_data(pkg_dir, data_subdir):
 description = 'Easily create cross-platform desktop applications with PyQt'
 setup(
     name='fbs',
-    version='0.3.9-SNAPSHOT',
+    version='0.3.9',
     description=description,
     long_description=
         description + '\n\nHome page: https://build-system.fman.io',
