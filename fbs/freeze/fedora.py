a063ba293716f1ca92495b717a7bb72e27eb16a5
Michael Herrmann <michael@herrmann.io>
2018-11-20 09:44:21 +0100

    Make fbs compatible with Python 3.6

--- fbs/freeze/fedora.py
+++ fbs/freeze/fedora.py
@@ -7,3 +7,8 @@ def freeze_fedora(extra_pyinstaller_args=None, debug=False):
     remove_shared_libraries('libgio-2.0.so.*', 'libglib-2.0.so.*')
     # Fixes for Fedora 29:
     remove_shared_libraries('libfreetype.so.*', 'libssl.so.*')
+    # PyInstaller 3.4 includes the library below when on Python 3.6.
+    # (Interestingly, it does not package it on Python 3.5.) This leads to a lot
+    # of Fontconfig-related errors when starting the frozen app. Further,
+    # starting the app takes ages. Removing the library fixes this:
+    remove_shared_libraries('libfontconfig.so.*')
--- setup.py
+++ setup.py
@@ -49,7 +49,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-    
+        'Programming Language :: Python :: 3.6',
+
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
