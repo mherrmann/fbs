3e967848959b73dbaee3fe09db452c93f51b449e
Michael Herrmann <michael@herrmann.io>
2018-11-26 09:13:23 +0100

    Fix problem running app frozen on Ubuntu 14 on 16

--- fbs/freeze/ubuntu.py
+++ fbs/freeze/ubuntu.py
@@ -13,3 +13,14 @@ def freeze_ubuntu(debug=False):
     # The difference between the two build outputs seems to be
     # libgpg-error.so.0. Removing it fixes the problem:
     remove_shared_libraries('libgpg-error.so.*')
+    # libgtk-3.so is present on every Ubuntu system. Make sure we don't ship it
+    # to avoid incompatibilities. In particular, running the frozen app with
+    # libgtk-3.so from Ubuntu 14 on Ubuntu 16 produces many Gtk warnings
+    # "Theme parsing error".
+    remove_shared_libraries('libgtk-3.so.*')
+    # We also don't want to ship libgio-2.0.so because it is usually present.
+    # What's more, if we ship libgio without libgtk, then segmentation faults
+    # occur when freezing on Ubuntu 14 and running on Ubuntu 16. The reason for
+    # this is that libgio depends on libgtk. Because we don't ship libgtk, this
+    # loads the user's libgtk, which is incompatible between Ubuntu 14 and 16.
+    remove_shared_libraries('libgio-2.0.so.*')
