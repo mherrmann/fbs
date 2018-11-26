95119151a7dbb05b27d4f7ecca3823fd42e42a2a
Michael Herrmann <michael@herrmann.io>
2018-11-26 06:39:34 +0100

    Improve command order shown for `fbs --help`

--- fbs/_state.py
+++ fbs/_state.py
@@ -3,10 +3,11 @@ This INTERNAL module is used to manage fbs's global state. Having it here, in
 one central place, allows fbs's test suite to manipulate the state to test
 various scenarios.
 """
+from collections import OrderedDict
 
 SETTINGS = {}
 LOADED_PROFILES = []
-COMMANDS = {}
+COMMANDS = OrderedDict()
 
 def get():
     return dict(SETTINGS), list(LOADED_PROFILES), dict(COMMANDS)
