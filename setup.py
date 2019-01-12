76e9cd51a1c7ad0e977eaf0e7fec7687022a572c
Michael Herrmann <michael@herrmann.io>
2019-01-12 10:20:09 +0700

    Bump version for next development iteration

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ description = 'Easily create cross-platform desktop applications with PyQt'
 setup(
     name='fbs',
     # Also update fbs/_defaults/requirements/base.txt when you change this:
-    version='0.6.4',
+    version='0.6.5-SNAPSHOT',
     description=description,
     long_description=
         description + '\n\nHome page: https://build-system.fman.io',
