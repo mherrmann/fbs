bfd29047a5dcfb654b4e1d90292749778a55a2da
Michael Herrmann <michael@herrmann.io>
2019-02-22 15:18:38 +0100

    Remove dynamic link to PyInstaller
    This prevents fbs from being bound by the GPL itself.

--- fbs/freeze/hooks/hook-shiboken2.py
+++ fbs/freeze/hooks/hook-shiboken2.py
@@ -1,3 +1,11 @@
-from PyInstaller.utils.hooks import collect_data_files
-datas = collect_data_files('shiboken2', include_py_files=True, subdir='support')
+from glob import glob
+from os.path import dirname, relpath, join
+
+import shiboken2.support
+
+_base_dir = dirname(shiboken2.support.__file__)
+_python_files = glob(join(_base_dir, '**', '*.py'), recursive=True)
+_site_packages = dirname(dirname(_base_dir))
+datas = [(f, relpath(dirname(f), _site_packages)) for f in _python_files]
+
 hiddenimports = ['typing']
