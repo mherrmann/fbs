5e2bf297775d9bd883fab3732cf7472150373178
Michael Herrmann <michael@herrmann.io>
2019-04-25 16:07:03 +0200

    Add PyInstaller as a dependency

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
             'fbs/installer/mac', 'create-dmg'
         )
     },
+    install_requires=['PyInstaller>=3.3'],
     extras_require={
         # Also update requirements.txt when you change this:
         'licensing': ['rsa>=3.4.2'],
