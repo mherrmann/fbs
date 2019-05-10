8dfd8990a26cdb5fa20418b40e2bd725db43180c
Michael Herrmann <michael@herrmann.io>
2019-05-10 09:11:06 +0200

    Set version number for release 0.8.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ description = 'Create cross-platform desktop applications with Python and Qt'
 setup(
     name='fbs',
     # Also update fbs/_defaults/requirements/base.txt when you change this:
-    version='0.8.1-SNAPSHOT',
+    version='0.8.1',
     description=description,
     long_description=
         description + '\n\nHome page: https://build-system.fman.io',
