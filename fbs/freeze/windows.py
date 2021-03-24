422ba91660ce2319f65fdcedd88e4b2a7a2f0d71
WhatTheServer <admin@whattheserver.com>
2021-03-23 23:04:28 -0400

    Update windows.py
    add version file for pyinstaller windows.

--- fbs/freeze/windows.py
+++ fbs/freeze/windows.py
@@ -8,6 +8,59 @@ import os
 import struct
 import sys
 
+def create_version_file_windows(company=None):
+    if company is None:
+        company = SETTINGS['author']
+
+    file_version_info = f"""# UTF-8
+#
+# For more details about fixed file info 'ffi' see:
+# http://msdn.microsoft.com/en-us/library/ms646997.aspx
+
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
+    # Set not needed items to zero 0. Must always contain 4 elements.
+    filevers=({SETTINGS['version'].replace('.', ',')},0),
+    prodvers=({SETTINGS['version'].replace('.', ',')},0),
+    # Contains a bitmask that specifies the valid bits 'flags'r
+    mask=0x3f,
+    # Contains a bitmask that specifies the Boolean attributes of the file.
+    flags=0x0,
+    # The operating system for which this file was designed.
+    # 0x4 - NT and there is no need to change it.
+    OS=0x40004,
+    # The general type of file.
+    # 0x1 - the file is an application.
+    fileType=0x1,
+    # The function of the file.
+    # 0x0 - the function is not defined for this fileType
+    subtype=0x0,
+    # Creation date and time stamp.
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'{company}'),
+        StringStruct(u'FileDescription', u'{SETTINGS['app_name']}'),
+        StringStruct(u'FileVersion', u'{SETTINGS['version']}.0'),
+        StringStruct(u'InternalName', u'{SETTINGS['app_name']}'),
+        StringStruct(u'LegalCopyright', u'© {SETTINGS['author']}. All rights reserved.'),
+        StringStruct(u'OriginalFilename', u'{SETTINGS['app_name']}.exe'),
+        StringStruct(u'ProductName', u'{SETTINGS['app_name']}'),
+        StringStruct(u'ProductVersion', u'{SETTINGS['version']}.0')])
+      ]),
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)"""
+
+    with open("file_version_info.txt", "w", encoding='utf-8') as outfile:
+        outfile.write(file_version_info)
+
+
 def freeze_windows(debug=False):
     args = []
     if not (debug or SETTINGS['show_console_window']):
@@ -15,6 +68,13 @@ def freeze_windows(debug=False):
         # We therefore only add it when we're not debugging.
         args.append('--windowed')
     args.extend(['--icon', path('src/main/icons/Icon.ico')])
+    create_version_file_windows()
+    # If Version file exists in repo root we want to make sure PyInstaller is passed it as an argument:
+    # https://pyinstaller.readthedocs.io/en/stable/usage.html#capturing-windows-version-data
+    file = path('file_version_info.txt')
+    # if file.exists():
+    #     args.extend(['--version-file', path('file_version_info.txt')])
+    args.extend(['--version-file', path('file_version_info.txt')])
     run_pyinstaller(args, debug)
     _restore_corrupted_python_dlls()
     _generate_resources()
